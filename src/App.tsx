0d4d7336e5f8fe5694a68bb28c697d28e7344e4c
javadEsl <javades755@gmail.com>
2023-08-24 16:09:22 +0430

    Added: <ToastContainer/>

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import {ThemeProvider} from "@mui/material";
 import {lightTheme} from "./theme";
 import Routs from "./routes/Routs";
 import store from "./redux/store/store";
+import {ToastContainer} from "react-toastify";
 
 const App = () => {
-  return (
-      <div className={"App"}>
-        <Provider store={store}>
-          <ThemeProvider theme={lightTheme}>
-            <Routs/>
-          </ThemeProvider>
-        </Provider>
-      </div>
-  );
+    return (
+        <div className={"App"}>
+            <Provider store={store}>
+                <ThemeProvider theme={lightTheme}>
+                    <Routs/>
+                    <ToastContainer/>
+                </ThemeProvider>
+            </Provider>
+        </div>
+    );
 };
 
 export default App;
