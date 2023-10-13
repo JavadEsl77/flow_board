175cf10b89035c3c9b4fc3558ea0eb7fca392a56
javadEsl <javades755@gmail.com>
2023-10-13 21:58:26 +0330

    Added: LocalizationProvider

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,21 @@ import {lightTheme} from "./theme";
 import Routs from "./routes/Routs";
 import store from "./redux/store/store";
 import {Slide, ToastContainer} from "react-toastify";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+
 
 const App = () => {
     return (
         <div className={"App"}>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
             <Provider store={store}>
                 <ThemeProvider theme={lightTheme}>
                     <Routs/>
                     <ToastContainer autoClose={2500} transition={Slide}/>
                 </ThemeProvider>
             </Provider>
+            </LocalizationProvider>
         </div>
     );
 };
